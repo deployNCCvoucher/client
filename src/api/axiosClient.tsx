b5b7901bb7e667b212c0c0fe95fcd2acb654fd15
Nguyen Nga <nganguyen48999@gmail.com>
2023-08-31 15:09:40 +0700

    update

--- src/api/axiosClient.tsx
+++ src/api/axiosClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://be-theta.vercel.app/api/",
+  baseURL: "https://be-mocha-ten.vercel.app/api",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
